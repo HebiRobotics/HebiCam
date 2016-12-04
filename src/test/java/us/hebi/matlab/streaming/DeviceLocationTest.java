8596f300c596fe0e53a2f3c266a67fc7e21f6be2
Florian Enner <florian@enner.org>
2016-12-04 17:18:53 -0500

    missed unstaged hunks in previous commit

--- src/test/java/us/hebi/matlab/streaming/DeviceLocationTest.java
+++ src/test/java/us/hebi/matlab/streaming/DeviceLocationTest.java
@@ -8,56 +8,76 @@ import static org.junit.Assert.*;
  * @author Florian Enner < florian @ hebirobotics.com >
  * @since 04 Dec 2016
  */
-public class CameraLocationTest {
+public class DeviceLocationTest {
 
     @Test
     public void isNumber() throws Exception {
-        assertTrue(new CameraLocation(1).isNumber());
-        assertTrue(new CameraLocation(2).isNumber());
-        assertTrue(new CameraLocation(3).isNumber());
-        assertFalse(new CameraLocation("http://github.com/video").isNumber());
-        assertFalse(new CameraLocation("/dev/0").isNumber());
+        assertTrue(new DeviceLocation(1).isNumber());
+        assertTrue(new DeviceLocation(2).isNumber());
+        assertTrue(new DeviceLocation(3).isNumber());
+        assertFalse(new DeviceLocation("http://github.com/video").isNumber());
+        assertFalse(new DeviceLocation("/dev/0").isNumber());
 
         try {
-            new CameraLocation(-1).isNumber();
+            new DeviceLocation(-1).isNumber();
             fail("Negative number (-1)");
-        }catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
         }
 
         try {
-            new CameraLocation(0).isNumber();
+            new DeviceLocation(0).isNumber();
             fail("Negative number (0)");
-        }catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
         }
 
         try {
-            new CameraLocation(2.3).isNumber();
+            new DeviceLocation(2.3).isNumber();
             fail("Float");
-        }catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
         }
 
     }
 
     @Test
-    public void isURL() throws Exception {
-        assertTrue(new CameraLocation("http://github.com/video").isUrl());
-        assertTrue(new CameraLocation("http://127.0.0.1/video").isUrl());
-        assertTrue(new CameraLocation("rtsp://10.10.10.1/video").isUrl());
-        assertFalse(new CameraLocation(1).isUrl());
-        assertFalse(new CameraLocation("/dev/usb0").isUrl());
+    public void isUrl() throws Exception {
+        assertTrue(new DeviceLocation("http://127.0.0.1:80/axis-cgi/mjpg/video.cgi").isUrl()); // Axis mjpeg
+        assertTrue(new DeviceLocation("rtsp://127.0.0.2/axis-media/media.amp").isUrl()); // Axis h264
+        assertTrue(new DeviceLocation("rtsp://127.0.0.3/media/video1").isUrl()); // Sony camera
+        assertTrue(new DeviceLocation("rtsp://127.0.0.4:554/live/ch00_0").isUrl()); // Misc ip camera
+        assertTrue(new DeviceLocation("http://github.com/video").isUrl()); // web address
+        assertTrue(new DeviceLocation("rtsp://10.10.10.1/video?user=x&pw=y").isUrl());
+        assertTrue(new DeviceLocation("rtsp://user:pw@10.10.10.1/video").isUrl());
+
+        assertFalse(new DeviceLocation(1).isUrl()); // Numbered device
+        assertFalse(new DeviceLocation("/home/user/name/myFile.avi").isUrl()); // Linux file
+        assertFalse(new DeviceLocation("/dev/video0").isUrl()); // Linux device
+        assertFalse(new DeviceLocation("C:/myFile.avi").isUrl()); // Windows file
     }
 
     @Test
-    public void isReachableURL() throws Exception {
-
+    public void hasUrlScheme() throws Exception {
+        assertTrue(new DeviceLocation("http://github.com/video").hasUrlScheme("http"));
+        assertTrue(new DeviceLocation("rtsp://user:pw@10.10.10.1/video").hasUrlScheme("rtsp"));
+        assertFalse(new DeviceLocation("rtsp://user:pw@10.10.10.1/video").hasUrlScheme("http"));
+        assertFalse(new DeviceLocation("/dev/usb0").hasUrlScheme("http"));
     }
 
+    /**
+     * Mostly omitted for now because I'm not sure how to best check it.
+     * new File(url) apparently doesn't complain for web-urls and
+     * .isFile() etc. may fail on Windows for paths that are valid
+     * on Linux.
+     *
+     * @throws Exception
+     */
     @Test
     public void isFile() throws Exception {
-        assertTrue(new CameraLocation("/dev/usb0").isFile());
-        assertFalse(new CameraLocation("http://github.com/video").isFile());
-        assertFalse(new CameraLocation("rtsp://10.10.10.1/video").isFile());
-        assertFalse(new CameraLocation(1).isFile());
+        /*
+        assertTrue(new DeviceLocation("/dev/usb0").isFile());
+        assertFalse(new DeviceLocation("http://github.com/video").isFile());
+        assertFalse(new DeviceLocation("rtsp://10.10.10.1/video").isFile());
+        */
+        assertFalse(new DeviceLocation(1).isFile());
     }
 
 }
